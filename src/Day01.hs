dc395ff39cf26241f3028b5fa180a0a32b96e7ab
nkraljevic <nkraljevic@combis.hr>
2025-12-01 19:45:20 +0100

    Solution 01

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,34 @@
 module Day01 where
 
+-- Coz the Either has the same constructors
+import Data.Foldable
 import Paths_aoc2025 (getDataFileName)
+import Prelude hiding (Left, Right)
+
+data Rotation = Left Int | Right Int deriving (Show, Eq)
+
+parseRotation :: String -> Rotation
+parseRotation [] = error "Empty rotation"
+parseRotation (direction : ammount) = case direction of
+  'L' -> Left $ read ammount
+  'R' -> Right $ read ammount
+  _ -> error "Unexpected direction"
+
+rotate :: Int -> Rotation -> Int
+rotate state (Right ammount) = (state + (ammount `mod` 100)) `mod` 100
+rotate state (Left ammount) = ((state + 100) - (ammount `mod` 100)) `mod` 100
+
+countZeroes :: (Int, Int) -> Rotation -> (Int, Int)
+countZeroes (zeroesCount, state) rotation = case rotate state rotation of
+    0 -> (zeroesCount + 1, 0)
+    x -> (zeroesCount, x)
+
+
+getPassword :: (Int,Int) -> [Rotation] -> (Int,Int)
+getPassword = foldl' countZeroes
 
 day01 :: IO ()
 day01 = do
   inputLines <- lines <$> (getDataFileName "day01-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 01"
+  let rotationList = fmap parseRotation inputLines
+  print $ fst $ getPassword (0,50) rotationList
